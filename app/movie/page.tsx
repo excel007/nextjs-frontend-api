6037c6e3ccb09fbca13779f54a7fecb6d21b8fb2
excel007 <eak.ekkachai@gmail.com>
2023-10-22 08:02:04 +0700

    commit

--- app/movie/page.tsx
+++ app/movie/page.tsx
@@ -30,11 +30,17 @@ export default async function Movie() {
             {/* {res.results.map((mov: MovieType) => <i>{mov.original_title}</i>)} */}
             <div className='grid gap-2 p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
                 {res.results.map((mov: MovieType) =>
-                    <div key={mov.id}>
-                        {mov.original_title}
+                    <div key={mov.id} className='h-90'>
+                        <div className='relative h-full'>
+                        {/* {mov.original_title} */}
                         <Link href={`/movie/${mov.id}`}>
-                        <Image src={`https://image.tmdb.org/t/p/original${mov.poster_path}`} width={800} height={800} alt={mov.title} className='rounded' />
+                            <Image src={`https://image.tmdb.org/t/p/original${mov.poster_path}`} width={800} height={800} alt={mov.title} className='rounded' />
                         </Link>
+                        <div className='absolute w-full bottom-0 px-4 py-2 rounded-b-xl bg-zinc-800'>
+                            <h2 className='text-cyan-200 text-center text-sm truncate'>{mov.title}</h2>
+                            {mov.release_date ? <p className='text-cyan-400 text-center text-xs truncate'>{mov.release_date}</p> : null}
+                        </div>
+                        </div>
                     </div>
                 )}
             </div>
